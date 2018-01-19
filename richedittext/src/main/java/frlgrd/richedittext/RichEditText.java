5de93e5c20ad9f2e45bbc7399d9d66ba984c0566
frlgrd <francois.legrand.fl@gmail.com>
2018-01-19 18:02:04 +0100

    Global reformat

--- richedittext/src/main/java/frlgrd/richedittext/RichEditText.java
+++ richedittext/src/main/java/frlgrd/richedittext/RichEditText.java
@@ -7,23 +7,23 @@ import android.widget.LinearLayout;
 
 public class RichEditText extends LinearLayout {
 
-    public RichEditText(Context context) {
-        super(context);
-        init();
-    }
+	public RichEditText(Context context) {
+		super(context);
+		init();
+	}
 
-    public RichEditText(Context context, @Nullable AttributeSet attrs) {
-        super(context, attrs);
-        init();
-    }
+	public RichEditText(Context context, @Nullable AttributeSet attrs) {
+		super(context, attrs);
+		init();
+	}
 
-    public RichEditText(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
-        super(context, attrs, defStyleAttr);
-        init();
-    }
+	public RichEditText(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+		super(context, attrs, defStyleAttr);
+		init();
+	}
 
-    private void init() {
-        inflate(getContext(), R.layout.rich_edit_text, this);
-    }
+	private void init() {
+		inflate(getContext(), R.layout.rich_edit_text, this);
+	}
 
 }
--- sample/src/main/java/sample/frlgrd/richedittext/MainActivity.java
+++ sample/src/main/java/sample/frlgrd/richedittext/MainActivity.java
@@ -7,9 +7,9 @@ import com.frlgrd.richedittext.R;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
 }
