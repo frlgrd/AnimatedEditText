b007ee127fffb235fa534e3a224490d75ff15f28
François Legrand <flegrand.prestataire@voyages-sncf.com>
2018-01-19 16:18:13 +0100

    Create base development environement

---
+++ richedittext/src/main/java/frlgrd/com/richedittext/RichEditText.java
@@ -0,0 +1,21 @@
+package frlgrd.com.richedittext;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
+public class RichEditText extends LinearLayout {
+
+    public RichEditText(Context context) {
+        super(context);
+    }
+
+    public RichEditText(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public RichEditText(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
